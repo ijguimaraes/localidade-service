f0e231289bd118a6fff432bbe08bedded2765feb
Igor <igor.guimaraes@itsstecnologia.com.br>
2019-10-07 20:44:37 -0300

    commit inicial

---
+++ src/test/java/br/com/evoluum/localidadeservice/LocalidadeServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.evoluum.localidadeservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LocalidadeServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
