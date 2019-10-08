5182bdefba3e3d99b69a871a4e5264ce95b60a61
Igor <igor.guimaraes@itsstecnologia.com.br>
2019-10-07 21:01:55 -0300

    inserção biblioteca lombok para criação automática de métodos para encapsulamento; criação dto localidade

---
+++ src/main/java/br/com/evoluum/localidadeservice/dto/LocalidadeDTO.java
@@ -0,0 +1,19 @@
+package br.com.evoluum.localidadeservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LocalidadeDTO {
+    private String idEstado;
+    private String siglaEstado;
+    private String regiaoNome;
+    private String nomeCidade;
+    private String nomeMesorregiao;
+    private String nomeFormatado; // {cidade/UF}
+}
